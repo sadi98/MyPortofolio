40e2bc4c4155bdecdea44ac865b5362ef7e6f9ad
sadi98 <sadinursholihin@gmail.com>
2023-11-13 13:27:32 +0700

    slicing skills page

---
+++ src/data/skills.js
@@ -0,0 +1,27 @@
+export const skills = [
+    {
+        logo:"logo-html5",
+        level:"Intermediate",
+        count:90,
+    },
+    {
+        logo:"logo-css3",
+        level:"Advanced",
+        count:80,
+    },
+    {
+        logo:"logo-react",
+        level:"Advanced",
+        count:80,
+    },
+    {
+        logo:"logo-nodejs",
+        level:"Beginner",
+        count:70,
+    },
+    {
+        logo:"logo-laravel",
+        level:"Advanced",
+        count:80,
+    }
+]
